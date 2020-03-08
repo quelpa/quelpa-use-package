a83ba631fd64741c28385bd6a1ea10929810d3bd
tsuucat <13937915+tsuu32@users.noreply.github.com>
2020-03-09 03:43:59 +0900

    update travis-ci

--- ci/run.sh
+++ ci/run.sh
@@ -6,31 +6,9 @@ export LC_ALL=en_US.UTF-8
 
 trap "rm -rf ~/.emacs.d/" EXIT
 
-if [ "$USER" == "vagrant" ]; then
-    dir=/vagrant
-else
-    dir="$PWD"
-fi
-
-pwd=`pwd`
-
-if ! test -e /usr/local/bin/emacs; then
-    {
-        >&2 echo "--- installing deps ---"
-        sudo apt-get update
-        sudo apt-get -y install build-essential wget git mercurial
-        sudo apt-get -y build-dep emacs23
-        wget http://ftp.gnu.org/gnu/emacs/emacs-25.3.tar.gz -O- | tar xz
-        cd emacs-25.3
-        >&2 echo "--- building emacs ---"
-        ./configure &&\
-            make &&\
-            sudo make install
-        sudo apt-get install -y -qq git
-    } > /dev/null
-fi
+dir="$PWD"
 
 echo "--- running tests ---"
 
-emacs --batch --eval "(setq travis-ci-dir \"$dir\")" --load $dir/ci/.emacs
+${EMACS} --batch --eval "(defvar top-dir \"$dir\")" --load $dir/ci/.emacs
 rm -rf ~/.emacs.d/
